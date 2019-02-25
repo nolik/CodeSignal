a8238ddff4c09e34414105cb74926281604cd6ca
nolik <Novik030692!>
2019-02-26 00:20:43 +0300

    array_change - solved.

--- src/task/array_change.rs
+++ src/task/array_change.rs
@@ -9,46 +9,15 @@ For inputArray = [1, 1, 1], the output should be
 arrayChange(inputArray) = 3.
 */
 
-//Expected output: 89510
-
-pub fn arrayChange(input_array: Vec<i32>) -> i32 {
-    let mut max = dbg!(input_array.iter().max().unwrap());
-    let mut index = dbg!(input_array
-        .iter()
-        .position(|element| element == max)
-        .unwrap());
-    let mut tail = input_array.len();
+pub fn arrayChange(mut input_array: Vec<i32>) -> i32 {
     let mut counter = 0;
-    // find max element in array and place index
-    //find local max / split array to 2 part / work for counting with right part
-    //call function on left part once again
 
-    while tail != 0 {
-        println!("counter before incremention = {:?}", counter);
-        counter += dbg!(get_count_of_sliced_array(
-            &input_array[index + 1..tail],
-            max,
-        ));
-        tail = index.clone();
-        if index != 0 {
-            max = dbg!(input_array[0..index].iter().max().unwrap());
-            index = dbg!(input_array[0..index]
-                .iter()
-                .position(|element| element == max)
-                .unwrap());
+    for item in 0..input_array.len() - 1 {
+        if input_array[item] >= input_array[item + 1] {
+            counter += input_array[item] + 1 - input_array[item + 1];
+            input_array[item + 1] = input_array[item] + 1;
         }
     }
 
-    counter
-}
-
-fn get_count_of_sliced_array(input_array: &[i32], max: &i32) -> i32 {
-    let mut counter = 0;
-    let mut local_max = max.clone();
-    for item in input_array {
-        local_max += 1;
-        counter += dbg!(local_max) - dbg!(item);
-        println!("counter in cycle = {:?}", counter);
-    }
     counter
 }
