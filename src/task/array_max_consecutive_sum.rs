a1891476d570e7045ba13c50f05f54588916d244
nolik <Novik030692!>
2019-04-23 01:26:53 +0300

    Nice elegant solution.

--- src/task/array_max_consecutive_sum.rs
+++ src/task/array_max_consecutive_sum.rs
@@ -1,20 +1,3 @@
 pub fn arrayMaxConsecutiveSum(inputArray: Vec<i32>, k: i32) -> i32 {
-    let mut sum = 0;
-    let mut first_sum_item = inputArray[0];
-    for int in 0..k {
-        sum += inputArray[int as usize];
-    }
-    let mut max = sum;
-
-    for index in k..inputArray.len() as i32 {
-        let current_item = inputArray[index as usize];
-        sum += current_item - first_sum_item;
-        first_sum_item = inputArray[(index - k + 1) as usize];
-
-        if sum > max {
-            max = sum;
-        }
-    }
-
-    max
+    inputArray.windows(k as usize).map(|window| window.iter().sum()).max().unwrap()
 }
